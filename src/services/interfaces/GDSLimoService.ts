2cbca57dcb2121b1bd0900a7cce9360bf3b20fd9
Houssam-95 <houssam.developpeur.95@gmail.com>
2025-06-25 11:51:44 +0200

    remove uncessary models

--- src/services/interfaces/GDSLimoService.ts
+++
@@ -1,6 +0,0 @@
-import { Mission } from "@/models/Mission";
-
-export default interface GDSLimoService {
-    getAllMissions: () => Mission[];
-    createMission: (mission: Mission) => Mission;
-}
--- src/services/interfaces/WayPlanService.ts
+++
@@ -1,6 +0,0 @@
-import { Mission } from "@/models/Mission";
-
-export default interface WayPlanService {
-    getAllRessources: () => Mission[];
-    createResource: () => Mission;
-}
